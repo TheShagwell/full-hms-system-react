31e0a22f30e3b5e1ba74869a2a93430e4742fb56
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-03-06 11:56:30 +0100

    Updated...

--- app/(root)/dashboard/layout.tsx
+++ app/(root)/dashboard/layout.tsx
@@ -4,20 +4,6 @@ import React from 'react'
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
   return (
-    // <main className='flex min-h-screen w-full bg-white'>
-    //     <div>
-    //       <Sidebar/>
-    //     </div>
-    //     <div className="flex-1 overflow-auto border-solid border-l-pink-500 border-2">
-    //         <div className="mb-8 flex">
-    //             <DashboardTopNav/>
-    //         </div>
-    //         <div className=''>
-    //         {children}
-
-    //         </div>
-    //     </div>
-    // </main>
       <main className='flex min-h-screen flex-col w-full bg-white lg:flex-row'>
         <Sidebar/>
         <div className="px-4 flex-1 border-solid overflow-auto lg:mt-0 lg:max-h-screen">
@@ -26,17 +12,6 @@ export default function DashboardLayout({children}: {children: React.ReactNode})
           </div>
           {children}
         </div>
-      </main> 
-      // <main className='flex min-h-screen flex-col w-full bg-white lg:flex-row'>
-      //   <Sidebar/>
-      //   <div className="px-4 flex flex-col h-screen overflow-auto border-solid border-l-pink-500 border-2">
-      //     <div className="mb-8 justify-start w-full">
-      //         <DashboardTopNav/>
-      //     </div>
-      //     <div className="flex-1 overflow-auto lg:mt-0 lg:max-h-screen">
-      //       {children}
-      //     </div>
-      //   </div>
-      // </main>
+      </main>
   )
 }
