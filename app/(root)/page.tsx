32ab71d5ef819c1e7914eda256dcdffa471ec045
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-03-07 02:40:32 +0100

    Updated...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,22 @@
 "use client"
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+
+  const navigate = useRouter();
+
   return (
-   <div className="w-full">Home</div>
+   <div className="w-full">
+   <button
+   onClick={
+    ()=>{
+      navigate.push('/sign-in')
+    }
+   }
+   >
+    navigate
+   </button>
+   </div>
   );
 }
