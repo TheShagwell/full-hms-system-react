b96f6f94ee19f3cfb05c5545ea56885cabd1bb10
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-11-20 03:15:13 +0100

    Updated...

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+declare interface PatientInfo {
+    name: string;
+    age: number;
+    gender: string;
+    phone: string;
+    address: string;
+    email: string;
+    insurance: string;
+    medicalHistory: string;
+    allergies: string;
+    medications: string;
+    familyHistory: string;
+    medicalRecords: string;
+    patientId: string;
+}
