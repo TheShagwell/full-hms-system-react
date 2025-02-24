87bfc1392ba2727e049d6045104aa1d1c8389d78
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-02-24 14:51:45 +0100

    Add route for doctor consultation in dashboard

--- config/routes.ts
+++ config/routes.ts
@@ -2,5 +2,6 @@ export const routes = {
     dashboard: {
         dashboard: '/dashboard',
         patientRegistration: '/dashboard/patient-registration',
+        doctorConsultation: '/dashboard/consultation',
     },
 }
